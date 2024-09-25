272a46bb1cb51f4b0482e48e78d6ccf78582ee1d
Monica <monicut3@gmail.com>
2024-09-25 12:16:33 +0200

    Feat: Add memeController, createMeme

---
+++ controllers/memeController.js
@@ -0,0 +1,15 @@
+import memeModel from "../models/memeModel.js";
+
+const memeController= {
+    createMeme: async (req, res) => {
+        try { 
+            const {name, image, date, author, stream, description} = req.body;
+            const newMeme = await memeModel.create ({name, image, date, author, stream, description});
+            res.status(201).json(newMeme);
+        }
+        catch (error){
+            console.error(error);
+            res.status(500).json({Message:"× Error al crear meme"});
+        }
+    }
+}
