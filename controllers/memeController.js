cd5ff408f88317e4b338cff1285b517df9a6af38
a-bac-0 <anca.bacria@thevallians.com>
2024-09-25 12:28:24 +0200

    add: create meme

---
+++ controllers/memeController.js
@@ -0,0 +1,16 @@
+import memeModel from '../models/memeModel.js'
+
+const memeController = {
+    createMeme: async (req,res) => {
+        try {
+            const {name, image, date, author, stream, description} = req.body;
+            const newMeme = await memeModel.create ({name, image, date, author, stream, description});
+            res.status(201).json(newMeme);
+            
+        }
+        catch (error){
+            console.error(error);
+            res.status(500).json8({Message:"Error al crear meme"});
+        }
+    }
+}
