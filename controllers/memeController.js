d65ca7bb6bb601013fddb8bb74fff4d555d2937b
juamrm <ajulianaamorim@gmail.com>
2024-09-26 12:51:08 +0200

    feat: add gellallmemes method

--- controllers/memeController.js
+++ controllers/memeController.js
@@ -1,15 +1,31 @@
 import memeModel from "../models/memeModel.js";
 
-const memeController= {
-    createMeme: async (req, res) => {
-        try { 
-            const {name, image, date, author, stream, description} = req.body;
-            const newMeme = await memeModel.create ({name, image, date, author, stream, description});
-            res.status(201).json(newMeme);
-        }
-        catch (error){
-            console.error(error);
-            res.status(500).json({Message:"❌ Error al crear meme"});
-        }
+const memeController = {
+  createMeme: async (req, res) => {
+    try {
+      const { name, image, date, author, stream, description } = req.body;
+      const newMeme = await memeModel.create({
+        name,
+        image,
+        date,
+        author,
+        stream,
+        description,
+      });
+      res.status(201).json(newMeme);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ Message: "❌ Error al crear meme" });
     }
-}
+  },
+  getAllMemes: async (req, res) => {
+    try {
+      const allMemes = await memeModel.findAll();
+      res.status(200).json(allMemes);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ Message: "❌ Error al obtener memes" });
+    }
+  },
+};
+export default memeController;
