0d971a2d48815a5c7efc55ee7add3580c81661e1
LorelizDev <loreliz.dev@gmail.com>
2024-09-26 15:30:19 +0200

    chore: create config.js for dotenv setup

---
+++ config.js
@@ -0,0 +1,10 @@
+import { config } from "dotenv";
+
+config();
+
+const DB_NAME = process.env.DB_NAME;
+const DB_USER = process.env.DB_USER;
+const DB_PASSWORD = process.env.DB_PASSWORD;
+const DB_HOST = process.env.DB_HOST;
+
+export { DB_NAME, DB_USER, DB_PASSWORD, DB_HOST };
--- database/db.js
+++ database/db.js
@@ -1,10 +1,11 @@
 import { Sequelize } from "sequelize";
 import dotenv from "dotenv";
+import { DB_HOST, DB_NAME, DB_PASSWORD, DB_USER } from "../config.js";
 
 dotenv.config();
 
-const db = new Sequelize("memeos_app", "root", process.env.PASSWORD, {
-  host: "localhost",
+const db = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
+  host: DB_HOST,
   dialect: "mysql",
   define: {
     timestamps: false,
